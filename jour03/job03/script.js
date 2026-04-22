4290361c2a2bd6d514128f342666ec250bd8bd0e
adrien meinier <adrien.meinier@laplateforme.io>
2026-04-22 14:44:09 +0200

    correction

--- jour03/job03/script.js
+++ jour03/job03/script.js
@@ -3,81 +3,100 @@ $(document).ready(function () {
     let locked = false;
 
     const $game = $("#game");
-    const $empty = $("#empty");
+    const empty = $("#empty")[0];
 
-    // shuffle tiles
-    function shuffle() {
-        let items = $("#game img").toArray();
+    let grid = [];
 
-        for (let i = items.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [items[i], items[j]] = [items[j], items[i]];
-        }
+    function init() {
 
-        $("#game").empty();
+        grid = [];
 
-        items.forEach(el => {
-            $("#game").append(el);
+        $("#game img").each(function () {
+            grid.push(this);
         });
 
-        $("#game").append($empty);
+        grid.push(empty);
+
+        shuffle();
+        render();
     }
 
-    // check if two tiles are adjacent
+    function shuffle() {
+
+        for (let i = grid.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [grid[i], grid[j]] = [grid[j], grid[i]];
+        }
+    }
+
+    function render() {
+
+        $game.empty();
+
+        for (let i = 0; i < grid.length; i++) {
+            $game.append(grid[i]);
+        }
+    }
+
+    function indexOf(el) {
+        return grid.indexOf(el);
+    }
+
     function isAdjacent(a, b) {
-        let posA = $(a).index();
-        let posB = $(b).index();
 
-        let rowA = Math.floor(posA / 3);
-        let colA = posA % 3;
+        const p1 = indexOf(a);
+        const p2 = indexOf(b);
 
-        let rowB = Math.floor(posB / 3);
-        let colB = posB % 3;
+        const r1 = Math.floor(p1 / 3);
+        const c1 = p1 % 3;
+
+        const r2 = Math.floor(p2 / 3);
+        const c2 = p2 % 3;
 
         return (
-            (Math.abs(rowA - rowB) === 1 && colA === colB) ||
-            (Math.abs(colA - colB) === 1 && rowA === rowB)
+            (Math.abs(r1 - r2) === 1 && c1 === c2) ||
+            (Math.abs(c1 - c2) === 1 && r1 === r2)
         );
     }
 
-    // check win condition
     function checkWin() {
-        let win = true;
 
-        $("#game img").each(function (i) {
-            if ($(this).data("order") != i + 1) {
-                win = false;
-            }
-        });
-
-        if (win) {
-            locked = true;
-
-            $("#game").after(`
-                <div id="winMessage">Vous avez gagné </div>
-                <button id="restart">Recommencer</button>
-            `);
+        for (let i = 0; i < 8; i++) {
+            if ($(grid[i]).data("order") != i + 1) return;
         }
+
+        locked = true;
+
+        $game.after("<div id='winMessage'>Vous avez gagné</div>");
+        $game.after("<button id='restart'>Recommencer</button>");
     }
 
-    // click on tiles
     $("#game").on("click", "img", function () {
+
         if (locked) return;
 
-        if (isAdjacent(this, $empty[0])) {
-            $(this).insertAfter($empty);
+        if (isAdjacent(this, empty)) {
+
+            const i1 = grid.indexOf(this);
+            const i2 = grid.indexOf(empty);
+
+            grid[i1] = empty;
+            grid[i2] = this;
+
+            render();
             checkWin();
         }
     });
 
-    // restart
     $(document).on("click", "#restart", function () {
+
         locked = false;
+
         $("#winMessage").remove();
         $("#restart").remove();
-        shuffle();
+
+        init();
     });
 
-    // Initial shuffle
-    shuffle();
+    init();
 });
