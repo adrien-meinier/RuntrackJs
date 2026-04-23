962bff6e80bce7522a8b98927041417b0aec9837
adrien meinier <adrien.meinier@laplateforme.io>
2026-04-23 15:36:57 +0200

    job00005 again and again

--- jour04/job05/script.js
+++ jour04/job05/script.js
@@ -1,20 +1,23 @@
-document.getElementById("get-quote").addEventListener("click", async () => {
-    try {
-        const response = await fetch("https://zenquotes.io/api/random");
+document.getElementById("get-quote").addEventListener("click", getQuote);
 
-        const data = await response.json();
+function getQuote() {
+    const url = "https://zenquotes.io/api/random";
+    const proxy = "https://corsproxy.io/?";
 
+    fetch(proxy + encodeURIComponent(url))
+        .then(response => response.json())
+        .then(data => {
+            const quote = data[0].q;
+            const author = data[0].a;
 
-        const quote = data[0];
-
-        document.getElementById("quote-box").innerHTML = `
-            <div>"${quote.q}"</div>
-            <div class="author">— ${quote.a}</div>
-        `;
-
-    } catch (error) {
-        document.getElementById("quote-box").innerHTML =
-            "Erreur lors du chargement de la citation.";
-        console.error(error);
-    }
-});
+            document.getElementById("quote-box").innerHTML = `
+                <div>"${quote}"</div>
+                <div class="author">— ${author}</div>
+            `;
+        })
+        .catch(error => {
+            document.getElementById("quote-box").textContent =
+                "Erreur lors du chargement.";
+            console.error(error);
+        });
+}
